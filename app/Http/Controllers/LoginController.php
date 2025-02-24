2450233024679dcda89e185dbbfcd5344edfe37a
alpat08 <rioalfattah6@gmail.com>
2025-02-24 13:56:37 +0700

    Login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -19,22 +21,52 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-        ],[
-            'name.required' => 'Nama wajib di isi',
-            'email.required' => 'Email wajib di isi',
-            'password.required' => 'Password wajib di isi',
-        ]);
+        try {
+            $request->validate([
+                'name' => 'required',
+                'email' => 'required',
+                'password' => 'required|min:8',
+            ],[
+                'name.required' => 'Nama wajib di isi',
+                'email.required' => 'Email wajib di isi',
+                'password.required' => 'Password wajib di isi',
+            ]);
+    
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+            ]);
+    
+            return redirect()->route('login')->with('success','Registrasi berhasil');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Registrasi gagal');
+        }
+    }
 
-        User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+    public function login(Request $request)
+    {
+        try {
 
-        return redirect()->route('login')->with('success','Registrasi berhasil');
+            $data = $request->validate([
+                'login' => 'required',
+                'password' => 'required|min:8'
+            ], [
+                'login.required' => 'Harus Di isi',
+                'password' => 'Harus Di isi'
+            ]);
+
+            $user = User::where('name', $request->login)
+            ->orWhere('email', $request->login)->first();
+
+            if (! $user || ! Hash::check($request->password, $user->password)) {
+                return redirect()->back()->with('error', 'Login gagal');
+            }
+
+            Auth::login($user);
+            return redirect()->back()->with('success', 'Berhasil login');
+        } catch(\Exception $e) {
+            return redirect()->back()->with('error', 'Login gagal');
+        }   
     }
 }
