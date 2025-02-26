b1e62801152a0bfd050bbe51c8eeaa39f0052b91
alpat08 <rioalfattah6@gmail.com>
2025-02-26 09:56:09 +0700

    Tes admin

--- resources/views/admin/buku/create.blade.php
+++ resources/views/admin/buku/create.blade.php
@@ -4,15 +4,16 @@
     <div class="card rounded-4 shadow-sm p-3">
         <div class="card-body">
             <h4 class="card-title">Tambah Buku</h4>
-            <form action="{{route('buku.store')}}" method="post" enctype="multipart/form-data">
+            <form action="{{ route('buku.store') }}" method="post" enctype="multipart/form-data">
                 @csrf
 
                 <div class="mb-3">
                     <label for="title" class="form-label">Judul Buku</label>
-                    <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" />
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+                        id="title" value="{{old('title')}}"/>
                     @error('title')
                         <div class="invalid-feedback">
-                            {{$message}}
+                            {{ $message }}
                         </div>
                     @enderror
                 </div>
@@ -20,32 +21,30 @@
                 <div class="mb-3">
                     <label for="author" class="form-label">Penulis</label>
                     <input type="text" class="form-control @error('author') is-invalid @enderror" name="author"
-                        id="author" />
+                        id="author" value="{{old('author')}}"/>
                     @error('author')
                         <div class="invalid-feedback">
-                            {{$message}}
+                            {{ $message }}
                         </div>
                     @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="deskripsi" class="form-label">Deskripsi</label>
-                    <textarea class="form-control @error('deskripsi') is-invalid @enderror" name="deskripsi" id="deskripsi"
-                        rows="3"></textarea>
+                    <textarea class="form-control @error('deskripsi') is-invalid @enderror" name="deskripsi" id="deskripsi" rows="3">{{ old('deskripsi') }}</textarea>
                     @error('deskripsi')
                         <div class="invalid-feedback">
-                            {{$message}}
+                            {{ $message }}
                         </div>
                     @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="isi" class="form-label">Isi Buku</label>
-                    <textarea class="form-control @error('isi') is-invalid @enderror" name="isi" id="isi"
-                        rows="3"></textarea>
+                    <textarea class="form-control @error('isi') is-invalid @enderror" name="isi" id="isi" rows="3" >{{ old('isi') }}</textarea>
                     @error('isi')
                         <div class="invalid-feedback">
-                            {{$message}}
+                            {{ $message }}
                         </div>
                     @enderror
                 </div>
@@ -68,9 +67,9 @@
                     <br>
                     @foreach ($genre as $item)
                         <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="genre_{{$item->id}}" name="genre[]"
-                                value="{{$item->id}}" />
-                            <label class="form-check-label" for="genre_{{$item->id}}">{{$item->name}}</label>
+                            <input class="form-check-input" type="checkbox" id="genre_{{ $item->id }}" name="genre[]"
+                                value="{{ $item->id }}" />
+                            <label class="form-check-label" for="genre_{{ $item->id }}">{{ $item->name }}</label>
                         </div>
                     @endforeach
                 </div>
@@ -83,13 +82,21 @@
                         name="image" id="image" />
                     @error('image')
                         <div class="invalid-feedback">
-                            {{$message}}
+                            {{ $message }}
                         </div>
                     @enderror
                 </div>
 
+                {{-- <div class="mb-3">
+                    <div id="extraImage"></div>
+                </div> <!-- Tempat menambahkan input file tambahan -->
 
-                <a href="{{route('buku.index')}}" class="btn btn-secondary">Back</a>
+                <div class="mb-3">
+                    <button type="button" onclick="addFileInput()" class="btn btn-outline-primary"><i
+                            class="bi bi-plus-lg"></i></button>
+                </div> --}}
+
+                <a href="{{ route('buku.index') }}" class="btn btn-secondary">Back</a>
                 <button type="submit" class="btn btn-success">Submit</button>
             </form>
         </div>
@@ -97,6 +104,48 @@
 
 
     <script>
+        // let fileIndex = 1; // Untuk memberi ID unik pada setiap input file tambahan
+
+        // function addFileInput() {
+        //     const extraImagesDiv = document.getElementById('extraImage');
+
+        //     // Buat elemen div pembungkus
+        //     const fileWrapper = document.createElement('div');
+        //     fileWrapper.setAttribute("id", "image-" + fileIndex);
+        //     fileWrapper.classList.add('mb-3', 'd-flex', 'gap-3');
+
+    
+
+        //     // Buat elemen input file
+        //     const fileInput = document.createElement('input');
+        //     fileInput.setAttribute("type", "file");
+        //     fileInput.setAttribute("name", "image[]");
+        //     fileInput.setAttribute("onchange", "preview()");
+        //     fileInput.classList.add('form-control');
+
+        //     // Buat tombol hapus
+        //     const removeButton = document.createElement('button');
+        //     removeButton.innerText = "Hapus";
+        //     removeButton.classList.add('btn', 'btn-danger');
+        //     removeButton.setAttribute("type", "button");
+        //     removeButton.onclick = function() {
+        //         fileWrapper.remove();
+        //     };
+
+        //     // Masukkan input file dan tombol hapus ke div pembungkus
+        //     fileWrapper.appendChild(fileInput);
+        //     fileWrapper.appendChild(removeButton);
+
+        //     // Tambahkan ke form
+        //     extraImagesDiv.appendChild(fileWrapper);
+
+        //     fileIndex++; // Tambah index agar unik
+        // }
+
+        // document.getElementById('form').addEventListener('submit', function() {
+        //     event.preventDefault();
+        // })
+
         function preview() {
             const image = document.querySelector('#image');
             const preview = document.querySelector('.preview');
@@ -106,7 +155,7 @@
             const reader = new FileReader();
             reader.readAsDataURL(image.files[0]);
 
-            reader.onload = function (oFREvent) {
+            reader.onload = function(oFREvent) {
                 preview.src = oFREvent.target.result;
             }
         }
--- resources/views/admin/buku/index.blade.php
+++ resources/views/admin/buku/index.blade.php
@@ -4,29 +4,30 @@
     <div class="card rounded-4 shadow-sm p-3">
         <div class="card-body">
             <h4 class="card-title">Buku</h4>
-            <a href="{{route('buku.create')}}" class="btn btn-success my-2">Tambah</a>
+            <a href="{{ route('buku.create') }}" class="btn btn-success my-2">Tambah</a>
             <div class="row">
                 @foreach ($buku as $item)
                     <div class="col-12 col-md-4">
-                        <div class="card my-2 rounded-3" style="max-width: 250px">
-                            <img src="{{asset('storage/' . $item->image)}}"
-                                style="max-width: 250px; height: 250px; background-size: cover" class="card-img-top" />
+                        <div class="card my-2 rounded-3" style="max-width: 280px">
+                            <img src="{{ asset('storage/' . $item->image) }}"
+                                style="max-width: 280px; height: 280px; background-size: cover" class="card-img-top" />
                             <div class="card-body">
                                 {{-- @dd($item->genres) --}}
-                                <h4 class="card-title">Judul: {{$item->title}}</h4>
+                                <h4 class="card-title">Judul: {{ $item->title }}</h4>
                                 <h4 class="card-subtitle fs-6 d-inline">
                                     Genre: {{ $item->genres->take(3)->pluck('name')->implode(', ') }}
                                 </h4>
                                 <p class="card-text">Deskripsi: <br> {{ Str::limit($item->deskripsi, '50') }}</p>
-                                <a href="{{route('buku.show', $item->id)}}" class="btn btn-sm btn-info my-1"><i
+                                <a href="{{ route('buku.show', $item->id) }}" class="btn btn-sm btn-info my-1"><i
                                         class="bi bi-eye"></i> Lihat</a>
-                                <a href="{{route('buku.edit', $item->id)}}" class="btn btn-sm btn-warning my-1"><i
+                                <a href="{{ route('buku.edit', $item->id) }}" class="btn btn-sm btn-warning my-1"><i
                                         class="bi bi-pencil-square"></i> Edit</a>
-                                <form action="{{route('buku.destroy', $item->id)}}" method="post" class="d-inline">
+                                <form action="{{ route('buku.destroy', $item->id) }}" method="post" class="d-inline">
                                     @csrf
                                     @method('delete')
                                     <button type="submit" class="btn btn-sm btn-danger"
-                                        onclick="return confirm('Yakin ingin menghapus buku ini?')"><i class="bi bi-trash"></i>
+                                        onclick="return confirm('Yakin ingin menghapus buku ini?')"><i
+                                            class="bi bi-trash"></i>
                                         Delete</button>
                                 </form>
                             </div>
@@ -36,5 +37,4 @@
             </div>
         </div>
     </div>
-
 @endsection
