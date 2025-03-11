4764e766f1f92e098f0fee2a7ffe5274236a6995
alpat08 <rioalfattah6@gmail.com>
2025-03-11 13:58:47 +0700

    longtext untuk isi cerita buku

--- database/migrations/2025_02_28_025706_create_ceritas_table.php
+++ database/migrations/2025_02_28_025706_create_ceritas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('ceritas', function (Blueprint $table) {
             $table->id();
-            $table->string('isi');
+            $table->longText('isi');
             $table->timestamps();
         });
     }
